5dae21cef00b0344d7b75c53c95e98b8a1a3958c
Łukasz Korecki <lukasz@coffeesounds.com>
2013-01-20 20:49:32 +0000

    init

---
+++ app.rb
@@ -0,0 +1,20 @@
+class CoffeeApp < Sinatra::Base
+  get '/' do
+     "hello!"
+  end
+
+  get '/coffees' do
+
+  end
+  get '/team' do
+
+  end
+
+  post '/update/:coffee/:user' do
+
+  end
+
+  get '/stats' do
+
+  end
+end
