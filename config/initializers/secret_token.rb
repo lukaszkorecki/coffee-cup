159090c614bd31ea241bf3bebc8dd064a0211f05
Łukasz Korecki <lukasz@coffeesounds.com>
2013-01-20 20:41:54 +0000

    ok

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CoffeeCup::Application.config.secret_token = '7dc1e9a2b0417fa7eeaee5f316f5bba5d9e2f51bbd84d9e2cd637a99125f1761a95ceb40eb0a887d74e82bf087170a8dd504ac1a4969e4ea015cf290443b653d'
