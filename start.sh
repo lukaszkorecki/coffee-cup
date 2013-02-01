cd2b6c75c5f3dad7afdd386d62a7b7f8061a7358
Łukasz Korecki <lukasz@coffeesounds.com>
2013-02-01 12:55:23 +0000

    tweak start script

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-env USERS=`ruby -ryaml -rjson -e "puts YAML::load_file('users.yml').to_json"` bundle exec rackup
+env USERS=`ruby -ryaml -rjson -e "puts YAML::load_file('users.yml').to_json"` bundle exec rackup $*
