798dc64265837aca8ffaeb743c65ecc5ff209c67
Łukasz Korecki <lukasz@coffeesounds.com>
2013-02-01 14:19:28 +0000

    support redis password

--- lib/redis.rb
+++ lib/redis.rb
@@ -1,7 +1,7 @@
 require 'redis'
 opts = if ENV['REDIS_HOST'] and ENV['REDIS_PORT']
          puts "Connecting to remote redis!"
-         { host: ENV['REDIS_HOST'], port: ENV['REDIS_PORT'] }
+         { host: ENV['REDIS_HOST'], port: ENV['REDIS_PORT']  password: ENV['REDIS_PASSWORD']}
        else
          nil
        end
