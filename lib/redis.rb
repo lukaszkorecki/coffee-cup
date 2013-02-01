6f867c64ef57a3900173c6fd65afb34fb0669065
Łukasz Korecki <lukasz@coffeesounds.com>
2013-02-01 13:10:45 +0000

    debug

--- lib/redis.rb
+++ lib/redis.rb
@@ -1,5 +1,6 @@
 require 'redis'
 opts = if ENV['REDIS_HOST'] and ENV['REDIS_PORT']
+         puts "Connecting to remote redis!"
          { host: ENV['REDIS_HOST'], port: ENV['REDIS_PORT'] }
        else
          nil
